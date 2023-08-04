50f7207735c0bdcfb5014ca9ec0fec0734e27c40
Julius Calahi <juliuscalahi@gmail.com>
2023-08-04 21:51:29 +0800

    completed sign up modal

--- client/src/components/signup/signup.js
+++ client/src/components/signup/signup.js
@@ -1,6 +1,8 @@
 import PropTypes from "prop-types";
-import classnames from "classnames";
 import * as Dialog from "@radix-ui/react-dialog";
+import { Text } from "../text";
+import { Button, BUTTON_STYLE } from "../../components/button";
+import colors from "../../styles/colors.module.scss";
 import classes from "./signup.module.scss";
 
 const propTypes = {
@@ -13,9 +15,26 @@ const SignUp = ({ open, onClose }) => {
       <Dialog.Portal>
         <Dialog.Overlay className={classes.overlay} />
         <Dialog.Content className={classes.content}>
-          <Dialog.Title>HAHA</Dialog.Title>
-          <Dialog.Description>ang magaling</Dialog.Description>
-          <Dialog.Close asChild />
+          <Text className={classes.title} color={colors.grey100}>
+            Sign up or Log in
+          </Text>
+          <Button className={classes.button} buttonStyle={BUTTON_STYLE.PRIMARY}>
+            <Text className={classes.buttonText} color={colors.primary25}>
+              sign in with Google
+            </Text>
+          </Button>
+          <Button
+            className={classes.button}
+            buttonStyle={BUTTON_STYLE.SECONDARY}
+          >
+            <Text className={classes.buttonText} color={colors.grey100}>
+              or sign up / log in with email
+            </Text>
+          </Button>
+          <Text className={classes.footerText}>
+            {`by signing up, you agree to AudioPen's
+            Policy and Terms of Use.`}
+          </Text>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
