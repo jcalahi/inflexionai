35eff28df04f86a7d67eaf46e538d9f4887fe334
jcalahi <juliuscalahi@gmail.com>
2023-08-04 19:48:11 +0800

    updates

--- client/src/components/signup/signup.js
+++ client/src/components/signup/signup.js
@@ -1,5 +1,19 @@
+import PropTypes from "prop-types";
+import classnames from "classnames";
 import classes from "./signup.module.scss";
 
-const SignUp = () => {};
+const propTypes = {
+  show: PropTypes.bool.isRequired,
+};
+
+const SignUp = ({ show }) => {
+  return (
+    <div className={classnames(classes.signup, { [classes.displayBlock]: show })}>
+      <div className={classes.signup_dialog}></div>
+    </div>
+  );
+};
+
+SignUp.propTypes = propTypes;
 
 export { SignUp };
--- client/src/pages/home/homePage.js
+++ client/src/pages/home/homePage.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import classnames from "classnames";
 import { BUTTON_STYLE, Button } from "../../components/button";
@@ -15,68 +16,77 @@ import RecordIcon from "../../assets/icons/record.svg";
 import classes from "./homePage.module.scss";
 import colors from "../../styles/colors.module.scss";
 import { MenuIcon } from "../../components/menuIcon/menuIcon";
+import { SignUp } from "../../components/signup/signup";
 
 const HomePage = () => {
+  const [showModal, setShowModal] = useState(false);
   const navigate = useNavigate();
 
   const gotoDemoPage = () => {
     navigate("/demo");
   };
 
+  const handleSignupClick = () => {
+    setShowModal(true);
+  };
+
   return (
-    <div className={classes.container}>
-      <div className={classnames(classes.flexBetween, classes.mt20)}>
-        <MenuIcon icon={AccountIcon} text="Account" />
-        <img src={ProductIcon} alt="product hunt icon" />
-        <MenuIcon icon={SettingsIcon} text="Settings" textPosition="right" />
-      </div>
-      <div className={classes.content}>
-        <div>
-          <Text className={classes.title} color={colors.grey100}>
-            AudioPen
-          </Text>
+    <>
+      <div className={classes.container}>
+        <div className={classnames(classes.flexBetween, classes.mt20)}>
+          <MenuIcon icon={AccountIcon} text="Account" />
+          <img src={ProductIcon} alt="product hunt icon" />
+          <MenuIcon icon={SettingsIcon} text="Settings" textPosition="right" />
         </div>
-        <div>
-          <img src={BarIcon} alt="bar icon" />
-        </div>
-        <div className={classes.my_14}>
-          <Text className={classes.subtitle} color={colors.grey100}>
-            Go from fuzzy thought to clear text.
-          </Text>
-          <Text className={classes.subtitle_secondary}> Fast.</Text>
-        </div>
-        <div className={classes.description}>
-          <Text className={classes.description_content}>
-            {`Just hit record. Then start talking.
+        <div className={classes.content}>
+          <div>
+            <Text className={classes.title} color={colors.grey100}>
+              AudioPen
+            </Text>
+          </div>
+          <div>
+            <img src={BarIcon} alt="bar icon" />
+          </div>
+          <div className={classes.my_14}>
+            <Text className={classes.subtitle} color={colors.grey100}>
+              Go from fuzzy thought to clear text.
+            </Text>
+            <Text className={classes.subtitle_secondary}> Fast.</Text>
+          </div>
+          <div className={classes.description}>
+            <Text className={classes.description_content}>
+              {`Just hit record. Then start talking.
             AudioPen will clean things up when you're done.`}
-          </Text>
-        </div>
-        <div className={classes.buttonGroup}>
-          <Button buttonStyle={BUTTON_STYLE.PRIMARY} onClick={() => {}}>
-            <Text className={classes.buttonText} color={colors.primary25}>
-              sign up / log in
             </Text>
-          </Button>
-          <Button buttonStyle={BUTTON_STYLE.SECONDARY} onClick={gotoDemoPage}>
-            <Text className={classes.buttonText} color={colors.grey100}>
-              see how it works
-            </Text>
-          </Button>
+          </div>
+          <div className={classes.buttonGroup}>
+            <Button buttonStyle={BUTTON_STYLE.PRIMARY} onClick={handleSignupClick}>
+              <Text className={classes.buttonText} color={colors.primary25}>
+                sign up / log in
+              </Text>
+            </Button>
+            <Button buttonStyle={BUTTON_STYLE.SECONDARY} onClick={gotoDemoPage}>
+              <Text className={classes.buttonText} color={colors.grey100}>
+                see how it works
+              </Text>
+            </Button>
+          </div>
+          <div className={classes.arrowLine}>
+            <img src={ArrowLineIcon} alt="arrow line icon" />
+          </div>
         </div>
-        <div className={classes.arrowLine}>
-          <img src={ArrowLineIcon} alt="arrow line icon" />
+        <div className={classes.flexBetween}>
+          <MenuIcon icon={UploadIcon} text="Upload Audio" />
+          <img src={RecordIcon} alt="record icon" width="80" height="80" />
+          <MenuIcon
+            icon={TextNoteIcon}
+            text="Rewrite a Text Note"
+            textPosition="right"
+          />
         </div>
       </div>
-      <div className={classes.flexBetween}>
-        <MenuIcon icon={UploadIcon} text="Upload Audio" />
-        <img src={RecordIcon} alt="record icon" width="80" height="80" />
-        <MenuIcon
-          icon={TextNoteIcon}
-          text="Rewrite a Text Note"
-          textPosition="right"
-        />
-      </div>
-    </div>
+    <SignUp show={showModal} />
+    </>
   );
 };
 
