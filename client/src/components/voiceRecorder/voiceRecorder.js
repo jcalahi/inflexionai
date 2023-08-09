940b3ee2246958a2b6a0fed8ba3056288951e05a
jcalahi <juliuscalahi@gmail.com>
2023-08-09 22:23:24 +0800

    code changes

--- client/src/components/voiceRecorder/voiceRecorder.js
+++ client/src/components/voiceRecorder/voiceRecorder.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { useAudioRecorder } from "react-audio-voice-recorder";
 import AudioMotionAnalyzer from "audiomotion-analyzer";
-import { Text } from "../../components/text";
+import { Text } from "../text";
 import RestartIcon from "../../assets/icons/restart.svg";
 import CloseIcon from "../../assets/icons/close.svg";
 import StopRecordIcon from "../../assets/icons/stop_record.svg";
@@ -23,26 +23,33 @@ const showAlert = (err) => {
 const VoiceRecorder = ({ show, onClose }) => {
   const [isRecording, setIsRecording] = useState(false);
   const [audioMotion, setAudioMotion] = useState();
-  const { mediaRecorder, recordingTime, startRecording, stopRecording } =
-    useAudioRecorder(
-      {
-        noiseSuppression: true,
-        echoCancellation: true,
-      },
-      showAlert
-    );
+
+  const {
+    mediaRecorder,
+    recordingBlob,
+    recordingTime,
+    startRecording,
+    stopRecording,
+  } = useAudioRecorder(
+    {
+      noiseSuppression: true,
+      echoCancellation: true,
+    },
+    showAlert
+  );
 
   useEffect(() => {
     const audioMotion = new AudioMotionAnalyzer(
       document.getElementById("audio"),
       {
-        gradient: "rainbow",
+        gradient: "steelblue",
         height: 60,
         overlay: true,
         bgAlpha: 0,
         showBgColor: true,
         showScaleY: false,
         showScaleX: false,
+        ledBars: true,
         mode: 4,
       }
     );
@@ -69,8 +76,15 @@ const VoiceRecorder = ({ show, onClose }) => {
         mediaRecorder.stream
       );
       audioMotion.connectInput(stream);
+      audioMotion.volume = 0;
     }
   }, [mediaRecorder, audioMotion]);
+
+  const handleRestartBtn = () => {
+    if (!isRecording) {
+      setIsRecording(true);
+    }
+  };
 
   const handleCloseBtn = () => {
     setIsRecording(false);
@@ -81,7 +95,7 @@ const VoiceRecorder = ({ show, onClose }) => {
     setIsRecording(false);
   };
 
-  console.log(mediaRecorder);
+  console.log({ mediaRecorder, recordingBlob });
 
   return (
     <div className={classes.container}>
@@ -92,7 +106,7 @@ const VoiceRecorder = ({ show, onClose }) => {
       </div>
       <div className={classes.audio} id="audio"></div>
       <div className={classes.flexContainer}>
-        <img src={RestartIcon} alt="restart icon" />
+        <img src={RestartIcon} alt="restart icon" onClick={handleRestartBtn} />
         <img src={CloseIcon} alt="close icon" onClick={handleCloseBtn} />
       </div>
       <img
